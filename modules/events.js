4cdd807921ad77479e309249eedd12d14522e1fb
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-11 00:22:02 +0300

    Update events.js

--- modules/events.js
+++ modules/events.js
@@ -1,27 +1,29 @@
 import {
-    listBook, newBook, contact, section1, section2, section3, logo, separator,
-}from'./variables.js'
+  section1,
+  section2,
+  section3,
+  logo,
+  separator,
+} from './variables.js';
 
 export function handleListBookClick() {
-    section1.style.display = '';
-    section2.style.display = 'none';
-    section3.style.display = 'none';
-    separator.style.display = 'none';
-    logo.innerHTML = 'Awesome Books';
-  }
-  
-  export function handleNewBookClick() {
-    section1.style.display = 'none';
-    section2.style.display = '';
-    section3.style.display = 'none';
-    logo.innerHTML = 'Awesome Books';
-  }
-  
-  export function handleContactClick() {
-    section1.style.display = 'none';
-    section2.style.display = 'none';
-    section3.style.display = '';
-    logo.innerHTML = 'Awesome Books';
-  }
-  
-  
+  section1.style.display = '';
+  section2.style.display = 'none';
+  section3.style.display = 'none';
+  separator.style.display = 'none';
+  logo.innerHTML = 'Awesome Books';
+}
+
+export function handleNewBookClick() {
+  section1.style.display = 'none';
+  section2.style.display = '';
+  section3.style.display = 'none';
+  logo.innerHTML = 'Awesome Books';
+}
+
+export function handleContactClick() {
+  section1.style.display = 'none';
+  section2.style.display = 'none';
+  section3.style.display = '';
+  logo.innerHTML = 'Awesome Books';
+}
